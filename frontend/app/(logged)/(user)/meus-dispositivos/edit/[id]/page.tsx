53fd42daae6cd7e72576be6e9dbc05ad66e885ea
KelvynLenis <52057929+KelvynLenis@users.noreply.github.com>
2025-01-30 20:59:37 -0300

    Update page.tsx

--- frontend/app/(logged)/(user)/meus-dispositivos/edit/[id]/page.tsx
+++ frontend/app/(logged)/(user)/meus-dispositivos/edit/[id]/page.tsx
@@ -1,13 +1,8 @@
 import { DeviceForm } from "@/components/Forms/DeviceForm";
 import { Device } from "@/utils/types";
-interface EditDeviceProps {
-  params: {
-    id: string;
-  };
-}
 
-export default async function EditDevice({ params }: EditDeviceProps) {
-  const { id } = params;
+export default async function EditDevice({params}: {params: Promise<{ id: string }>}) {
+  const { id } = await params;
 
   if (!process.env.NEXT_PUBLIC_API_URL || !process.env.NEXT_PUBLIC_DATABASE_ID) {
     throw new Error('Configuração incompleta: verifique as variáveis de ambiente.');
