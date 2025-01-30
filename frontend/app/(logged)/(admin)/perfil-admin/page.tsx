3b5964963e63900c65094f9583c405fb69d136db
KelvynLenis <52057929+KelvynLenis@users.noreply.github.com>
2025-01-30 20:38:24 -0300

    Update page.tsx

--- frontend/app/(logged)/(admin)/perfil-admin/page.tsx
+++ frontend/app/(logged)/(admin)/perfil-admin/page.tsx
@@ -1,12 +1,10 @@
-import { EditPerfilForm } from "@/components/EditPerfilForm";
-import ProtectedRoute from "@/components/ProtectedRoute";
 
 export default async function Perfil() {
 
   return (
     <>
       <div className="w-full flex items-center justify-center">
-        <EditPerfilForm />
+        
       </div>
     </>
   )
