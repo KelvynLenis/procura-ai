2348ab105a1065e9bc7db34820e64dfbe174ca3a
KelvynLenis <kmartinslenis30@hotmail.com>
2025-02-03 20:48:53 -0300

    style: fixing styles

--- frontend/components/Tables/DevicesTable.tsx
+++ frontend/components/Tables/DevicesTable.tsx
@@ -80,7 +80,7 @@ export function DevicesTable() {
       <TableHeader className="bg-zinc-200/60">
         <TableRow>
           <TableHead className="text-black/80 pl-5 font-semibold hidden lg:table-cell w-20">ID</TableHead>
-          <TableHead className="text-black/80 font-semibold lg:flex w-44 items-center">Modelo</TableHead>
+          <TableHead className="text-black/80 font-semibold flex w-28 sm:justify-center md:flex items-center">Modelo</TableHead>
           <TableHead className="text-black/80 font-semibold hidden md:table-cell w-1/3">Marca</TableHead>
           <TableHead className="text-black/80 font-semibold hidden md:table-cell lg:w-full">IMEI</TableHead>
           <TableHead className="text-black/80 font-semibold w-24 text-center">Status</TableHead>
