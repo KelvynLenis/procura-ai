460da4a8fcf8f7f6b283b85b67e870146fac9814
KelvynLenis <kmartinslenis30@hotmail.com>
2025-02-05 15:47:17 -0300

    feat: features section updated

--- frontend/components/Features.tsx
+++ frontend/components/Features.tsx
@@ -13,40 +13,36 @@ export function Features() {
 
   return (
     <section className="w-full h-screen relative flex flex-col items-center justify-center px-4 z-0">
-      <Image src={shapesLeft} alt="formas esquerda" className="absolute -top-14 left-0 z-0" />
-      <Image src={shapesRight} alt="formas direita" className="absolute -bottom-1/4 right-0 z-0" />
+      <Image src={shapesLeft} alt="formas esquerda" className="absolute bottom-24 left-0 z-0" />
+      <Image src={shapesRight} alt="formas direita" className="absolute top-80 -right-0" />
 
       <div className="flex md:flex-row flex-col lg:gap-10 z-10 gap-5 md:px-10">
         <div className="md:flex justify-center items-center hidden">
           <div className="flex flex-col lg:w-[390px]">
             <Image src={arrow} alt="arrow" className="rotate-90 md:rotate-0" />
-            <span className="lg:text-4xl text-2xl font-medium">Veja a que é possível fazer através do Procura.Aí</span>
+            <span className="lg:text-4xl text-2xl font-medium">Com o Procura.Aí você pode</span>
           </div>
         </div>
 
-        <div className="w-full lg:w-[500px] xl:h-[400px] bg-primary/5 border border-primary rounded-xl mb-10 lg:mb-20">
-          <ul className="font-medium text-lg flex flex-col items-center h-full pt-5">
+        <div className="w-full bg-primary/5 border border-primary rounded-xl mb-10 lg:mb-20 p-6">
+          <ul className="font-medium text-lg flex flex-col items-center h-full gap-6">
             <li className="flex items-center w-full">
               <Image src={block} alt="celular cortado" height={70} />
-              <span>Bloqueio remoto do aparelho</span>
+              <span>Fazer o bloqueio remoto do dispositivo</span>
             </li>
             <li className="flex items-center w-full">
               <Image src={notify} alt="celular notificando" height={70} />
-              <span>Notificação de alerta</span>
+              <span>Notificar as autoridades</span>
             </li>
             <li className="flex items-center w-full">
               <Image src={location} alt="alfinete de mapa" height={70} />
-              <span>Rastreio do aparelho</span>
-            </li>
-            <li className="flex items-center w-full">
-              <Image src={dollar} alt="simbolo de dollar" height={70} />
-              <span>Bloqueio de aplicativos bancários</span>
+              <span>Rastrear a localização do dispositivo</span>
             </li>
           </ul>
         </div>
       </div>
 
-      <span className="font-bold mb-4 text-lg z-10">Baixe nas principais lojas de aplicativos</span>
+      {/* <span className="font-bold mb-4 text-lg z-10">Baixe nas principais lojas de aplicativos</span>
       <div className="flex z-10">
         <button>
           <Image src={googlePlay} alt="disponível no google play" />
@@ -54,7 +50,7 @@ export function Features() {
         <button>
           <Image src={appStore} alt="disponível na app store" />
         </button>
-      </div>
+      </div> */}
 
     </section>
   )
