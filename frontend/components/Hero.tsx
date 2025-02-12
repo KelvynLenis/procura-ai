772c1452004b2116565c7d0d9cb300bdae703d09
KelvynLenis <kmartinslenis30@hotmail.com>
2025-02-12 08:28:42 -0300

    style: fix hero text style

--- frontend/components/Hero.tsx
+++ frontend/components/Hero.tsx
@@ -9,15 +9,12 @@ export function Hero() {
     <section className="z-100 w-full bg-zinc-100 flex items-center justify-between h-full text-lg md:text-xl lg:text-4xl text-white px-2 md:px-5 lg:px-10 py-7 z-10">
       <Image src={landingImages} alt="landing background" className="w-1/2 hidden lg:block" />
       <div className="flex flex-col items-end gap-4 lg:gap-5">
-        <span className="font-bold w-9/12 text-end text-xl lg:text-5xl text-primary">
-          Mais segurança na
-          Paraíba
+        <span className="font-bold w-[80.5%] text-end text-xl lg:text-5xl text-primary">
+          Perdeu ou teve seu celular roubado? O <span className="text-white bg-primary rounded-lg px-2 py-0">Procura.AÍ</span>pode te ajudar
         </span>
         <span className="font-semibold lg:text-2xl break-words w-9/12 text-procura-ai-black text-end">
-          Perdeu ou teve seu celular roubado? <br />
-          O Procura.AÍ pode te ajudar! <br />
           Cadastre seus dispositivos e, se algo acontecer, acione as autoridades de forma rápida e segura. <br />
-          Com ajuda da tecnologia, você aumenta as chances de recuperar seu aparelho e ainda contribui para combater o mercado ilegal. <br />
+          Com ajuda da tecnologia, você aumenta as chances de recuperar seu aparelho e ainda contribui para combater o mercado ilegal.
           Proteja-se agora e fique um passo à frente
         </span>
         <Link href={'/login'} className="self-center flex lg:self-end">
