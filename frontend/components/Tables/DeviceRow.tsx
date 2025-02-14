18004acc6639aa6a3ddbe61e531f24e7e32dcb4c
KelvynLenis <kmartinslenis30@hotmail.com>
2025-02-14 16:20:35 -0300

    fix: remove unused code

--- frontend/components/Tables/DeviceRow.tsx
+++ frontend/components/Tables/DeviceRow.tsx
@@ -145,7 +145,6 @@ export function DeviceRow({ id, phone_number, phone_model, brand, imei, isStolen
         setDevices((prevDevices) => prevDevices.map((device) => device.$id === id ? { ...device, is_stolen: false, status: "Recuperado" } : device));
 
       }
-      // router.refresh()
     } catch (error) {
       console.error(error)
     }
@@ -157,127 +156,129 @@ export function DeviceRow({ id, phone_number, phone_model, brand, imei, isStolen
 
 
   return (
-    <TableRow className="text-base">
-      <TableCell className="font-bold text-zinc-800 pl-5 hidden lg:table-cell">{index + 1}</TableCell>
-      <TableCell className="font-bold text-zinc-800 lg:flex">{phone_model}</TableCell>
-      <TableCell className="font-bold capitalize hidden md:table-cell">{brand}</TableCell>
-      <TableCell className="font-bold hidden md:table-cell">{imei.slice(0, 1) + ' ' + imei.slice(1, 8) + ' ****** **'}</TableCell>
-      <TableCell className="w-24">
-        <span className={cn("rounded-md w-20 flex items-center justify-center capitalize",
-          status === "Roubado" && "bg-robbery-bg text-robbery-text p-1",
-          status === "Recuperado" && "bg-regular-bg text-regular-text p-1",
-          status === "Regular" && "bg-regular-bg text-regular-text p-1",
-          status === "Furtado" && "bg-theft-bg text-theft-text p-1",
-          status === "Perdido" && "bg-lost-bg text-lost-text p-1",
-          // status === "Perdido" && "bg-violet-500/20 text-violet-700 p-1",
-        )}>{status === 'Recuperado' ? "Regular" : status.replace(' ', '')}</span>
-      </TableCell>
-      <TableCell className="flex gap-2 items-center h-20 py-28 md:py-10 mdflex-wrap md:my-3">
-        <div className="flex flex-col md:flex-row items-center w-full gap-2">
+    <>
+      <TableRow className="text-base">
+        <TableCell className="font-bold text-zinc-800 pl-5 hidden lg:table-cell">{index + 1}</TableCell>
+        <TableCell className="font-bold text-zinc-800 lg:flex">{phone_model}</TableCell>
+        <TableCell className="font-bold capitalize hidden md:table-cell">{brand}</TableCell>
+        <TableCell className="font-bold hidden md:table-cell">{imei.slice(0, 1) + ' ' + imei.slice(1, 8) + ' ****** **'}</TableCell>
+        <TableCell className="w-24">
+          <span className={cn("rounded-md w-20 flex items-center justify-center capitalize",
+            status === "Roubado" && "bg-robbery-bg text-robbery-text p-1",
+            status === "Recuperado" && "bg-regular-bg text-regular-text p-1",
+            status === "Regular" && "bg-regular-bg text-regular-text p-1",
+            status === "Furtado" && "bg-theft-bg text-theft-text p-1",
+            status === "Perdido" && "bg-lost-bg text-lost-text p-1",
+            // status === "Perdido" && "bg-violet-500/20 text-violet-700 p-1",
+          )}>{status === 'Recuperado' ? "Regular" : status.replace(' ', '')}</span>
+        </TableCell>
+        <TableCell className="flex gap-2 items-center h-20 py-28 md:py-10 mdflex-wrap md:my-3">
+          <div className="flex flex-col md:flex-row items-center w-full gap-2">
 
-          <Dialog>
-            <DialogTrigger asChild>
-              <button className="rounded-lg w-10 h-10 flex ring-1 ring-zinc-300 group relative hover:bg-sky-100 hover:ring-blue-700 hover:text-blue-900 items-center justify-center hover:opacity-90">
-                <Eye size={26} />
+            <Dialog>
+              <DialogTrigger asChild>
+                <button className="rounded-lg w-10 h-10 flex ring-1 ring-zinc-300 group relative hover:bg-sky-100 hover:ring-blue-700 hover:text-blue-900 items-center justify-center hover:opacity-90">
+                  <Eye size={26} />
+                  <span className="hidden opacity-0 group-hover:block group-hover:opacity-100 bg-black/60 w-36 rounded-sm absolute -top-8 right-5 py-1 text-white transition- duration-300">
+                    Exibir informações
+                  </span>
+                </button>
+              </DialogTrigger>
+              <DialogContent className="flex flex-col py-10 gap-10">
+                <DialogHeader>
+                  <DialogTitle>Detalhes do usuários</DialogTitle>
+                </DialogHeader>
+
+                <div className="flex gap-8">
+
+                  <div className="flex flex-col items-start justify-center">
+                    <span className="font-bold">Número</span>
+                    <span className="break-words">{phone_number}</span>
+                  </div>
+
+                  <div className="flex flex-col items-start justify-center">
+                    <span className="font-bold">Modelo</span>
+                    <span>{phone_model}</span>
+                  </div>
+
+                  <div className="flex flex-col gap-2 items-center justify-start">
+                    <span className="font-bold">Brand</span>
+                    <span>{brand}</span>
+                  </div>
+
+
+                  <div className="flex flex-col gap-2 items-center justify-start">
+                    <span className="font-bold">IMEI</span>
+                    <span>{imei}</span>
+                  </div>
+
+
+                  <div className="flex flex-col gap-2 items-center justify-start">
+                    <span className="font-bold">Status</span>
+                    <span>{status}</span>
+                  </div>
+
+                </div>
+
+              </DialogContent>
+            </Dialog>
+
+
+
+            <Link href={`meus-dispositivos/edit/${id}`}>
+              <button onClick={showLoadingToast} className="rounded-lg w-10 h-10 flex ring-1 ring-zinc-300 group relative hover:bg-sky-100 hover:ring-blue-700 hover:text-blue-900 items-center justify-center hover:opacity-90">
+                <ImPencil size={16} />
                 <span className="hidden opacity-0 group-hover:block group-hover:opacity-100 bg-black/60 w-36 rounded-sm absolute -top-8 right-5 py-1 text-white transition- duration-300">
-                  Exibir informações
+                  Editar dispositivo
                 </span>
               </button>
-            </DialogTrigger>
-            <DialogContent className="flex flex-col py-10 gap-10">
-              <DialogHeader>
-                <DialogTitle>Detalhes do usuários</DialogTitle>
-              </DialogHeader>
+            </Link>
 
-              <div className="flex gap-8">
-
-                <div className="flex flex-col items-start justify-center">
-                  <span className="font-bold">Número</span>
-                  <span className="break-words">{phone_number}</span>
-                </div>
-
-                <div className="flex flex-col items-start justify-center">
-                  <span className="font-bold">Modelo</span>
-                  <span>{phone_model}</span>
-                </div>
-
-                <div className="flex flex-col gap-2 items-center justify-start">
-                  <span className="font-bold">Brand</span>
-                  <span>{brand}</span>
-                </div>
-
-
-                <div className="flex flex-col gap-2 items-center justify-start">
-                  <span className="font-bold">IMEI</span>
-                  <span>{imei}</span>
-                </div>
-
-
-                <div className="flex flex-col gap-2 items-center justify-start">
-                  <span className="font-bold">Status</span>
-                  <span>{status}</span>
-                </div>
-
-              </div>
-
-            </DialogContent>
-          </Dialog>
-
-
-
-          <Link href={`meus-dispositivos/edit/${id}`}>
-            <button onClick={showLoadingToast} className="rounded-lg w-10 h-10 flex ring-1 ring-zinc-300 group relative hover:bg-sky-100 hover:ring-blue-700 hover:text-blue-900 items-center justify-center hover:opacity-90">
-              <ImPencil size={16} />
+            <button className="rounded-lg w-10 h-10 flex group relative items-center justify-center gap-2 ring-1 ring-zinc-300 hover:bg-red-200 hover:ring-red-600 text-red-600 hover:opacity-90" onClick={() => handleDeleteDevice(id)}>
+              <Trash2 size={20} />
               <span className="hidden opacity-0 group-hover:block group-hover:opacity-100 bg-black/60 w-36 rounded-sm absolute -top-8 right-5 py-1 text-white transition- duration-300">
-                Editar dispositivo
+                Deletar dispositivo
               </span>
             </button>
-          </Link>
 
-          <button className="rounded-lg w-10 h-10 flex group relative items-center justify-center gap-2 ring-1 ring-zinc-300 hover:bg-red-200 hover:ring-red-600 text-red-600 hover:opacity-90" onClick={() => handleDeleteDevice(id)}>
-            <Trash2 size={20} />
-            <span className="hidden opacity-0 group-hover:block group-hover:opacity-100 bg-black/60 w-36 rounded-sm absolute -top-8 right-5 py-1 text-white transition- duration-300">
-              Deletar dispositivo
-            </span>
-          </button>
+            {
+              isStolen
+                ? <button title="Desativar alerta" className={cn(
+                  "w-10 h-10 group relative rounded-lg flex flex-col md:flex-row items-center justify-center hover:bg-white",
+                  status === "Roubado" && "bg-robbery-bg text-red-600 p-1 ring-1 ring-red-500",
+                  status === "Furtado" && "bg-theft-bg text-orange-600 p-1 ring-1 ring-orange-500",
+                  status === "Perdido" && "bg-lost-bg text-yellow-600 p-1 ring-1 ring-yellow-500",
+                  status === "Recuperado" && "bg-lime-500/30 text-lime-600 p-1 ring-1 ring-lime-500",
+                  status === "Regular" && "bg-lime-500/30 text-lime-600 p-1 ring-1 ring-lime-500",
+                )} onClick={() => handleDeviceRecovery(id)}>
+                  <IoIosWarning size={28} />
+                  <span className="hidden opacity-0 group-hover:block group-hover:opacity-100 bg-black/60 w-32 rounded-sm absolute -top-8 right-5 py-1 px-2 text-white transition- duration-300">
+                    Desativar alerta
+                  </span>
+                </button>
 
-          {
-            isStolen
-              ? <button title="Desativar alerta" className={cn(
-                "w-10 h-10 group relative rounded-lg flex flex-col md:flex-row items-center justify-center hover:bg-white",
-                status === "Roubado" && "bg-robbery-bg text-red-600 p-1 ring-1 ring-red-500",
-                status === "Furtado" && "bg-theft-bg text-orange-600 p-1 ring-1 ring-orange-500",
-                status === "Perdido" && "bg-lost-bg text-yellow-600 p-1 ring-1 ring-yellow-500",
-                status === "Recuperado" && "bg-lime-500/30 text-lime-600 p-1 ring-1 ring-lime-500",
-                status === "Regular" && "bg-lime-500/30 text-lime-600 p-1 ring-1 ring-lime-500",
-              )} onClick={() => handleDeviceRecovery(id)}>
-                <IoIosWarning size={28} />
-                <span className="hidden opacity-0 group-hover:block group-hover:opacity-100 bg-black/60 w-32 rounded-sm absolute -top-8 right-5 py-1 px-2 text-white transition- duration-300">
-                  Desativar alerta
-                </span>
-              </button>
+                :
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <button className={cn("rounded-lg group relative w-10 h-10 ring-1 ring-zinc-300 flex flex-col md:flex-row items-center justify-center text-red-600 hover:bg-red-300 hover:ring-red-500")}>
+                      <IoIosWarning size={28} />
+                      <span className="hidden opacity-0 group-hover:block group-hover:opacity-100 bg-black/60 w-28 rounded-sm absolute -top-8 right-5 py-1 text-white transition- duration-300">
+                        Acionar alerta
+                      </span>
+                    </button>
+                  </DialogTrigger>
+                  <DialogContent className="flex flex-col h-4/5 md:h-fit overflow-y-scroll w-fit py-8">
 
-              :
-              <Dialog>
-                <DialogTrigger asChild>
-                  <button className={cn("rounded-lg group relative w-10 h-10 ring-1 ring-zinc-300 flex flex-col md:flex-row items-center justify-center text-red-600 hover:bg-red-300 hover:ring-red-500")}>
-                    <IoIosWarning size={28} />
-                    <span className="hidden opacity-0 group-hover:block group-hover:opacity-100 bg-black/60 w-28 rounded-sm absolute -top-8 right-5 py-1 text-white transition- duration-300">
-                      Acionar alerta
-                    </span>
-                  </button>
-                </DialogTrigger>
-                <DialogContent className="flex flex-col h-4/5 md:h-fit overflow-y-scroll w-fit py-8">
-
-                  <DialogHeader>
-                    <DialogTitle>Preencha as informações</DialogTitle>
-                  </DialogHeader>
-                  <MarkAsStolenForm id={id} isStolen={isStolen} setDevices={setDevices} />
-                </DialogContent>
-              </Dialog>
-          }
-        </div>
-      </TableCell>
-    </TableRow >
+                    <DialogHeader>
+                      <DialogTitle>Preencha as informações</DialogTitle>
+                    </DialogHeader>
+                    <MarkAsStolenForm id={id} isStolen={isStolen} setDevices={setDevices} />
+                  </DialogContent>
+                </Dialog>
+            }
+          </div>
+        </TableCell>
+      </TableRow>
+    </>
   )
 }
