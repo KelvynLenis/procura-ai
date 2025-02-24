f17ff8597377c42e8f3c9b6ab58b67f8012da6bd
KelvynLenis <kmartinslenis30@hotmail.com>
2025-02-24 09:35:21 -0300

    feat: add responsiveness to devices table on mobile

--- frontend/components/Button.tsx
+++ frontend/components/Button.tsx
@@ -17,7 +17,6 @@ export default function Button({ children, variant, isLoader, className, ...prop
   return (
     <button className={cn(
       "rounded-full text-center items-center justify-center flex w-fit px-2 py-2 drop-shadow-lg transition-all duration-300 disabled:bg-zinc-300 disabled:text-zinc-400 disabled:ring-0",
-      variant === 'orange' && "bg-primary text-white hover:bg-white hover:text-primary hover:ring-1 hover:ring-primary",
       variant === 'blue' && "bg-procura-ai-blue text-white hover:bg-white hover:text-procura-ai-blue hover:ring-1 hover:ring-procura-ai-blue",
       variant === 'white' && "bg-white border-[0.5px] border-primary text-primary hover:bg-primary hover:text-white",
       variant === 'red' && "bg-red-500 border-[0.5px] border-red-500 text-white hover:bg-white hover:text-red-500",
