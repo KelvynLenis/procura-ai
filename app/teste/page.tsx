48e081434427087dcc2d256264239571f11672c3
KelvynLenis <52057929+KelvynLenis@users.noreply.github.com>
2025-03-12 20:04:15 -0300

    Fix Vercel deploy error

--- app/teste/page.tsx
+++ app/teste/page.tsx
@@ -1,11 +1,11 @@
-import { TestComponent } from '@/components/TestComponent';
+// import { TestComponent } from '@/components/TestComponent';
 
 
 export default function Page() {
   return (
     <>
       <main className="flex flex-colitems-center justify-center min-h-screen w-screen p-10">
-        <TestComponent  />
+         {/* <TestComponent /> */}
       </main>
     </>
   );
