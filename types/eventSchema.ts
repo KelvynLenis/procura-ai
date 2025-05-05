a757efd2b94a1839a6fd27fd2dd16a7ac8772772
KelvynLenis <kmartinslenis30@hotmail.com>
2025-05-05 15:03:13 -0300

    feat: add informations to event

--- types/eventSchema.ts
+++ types/eventSchema.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import { z } from 'zod'
 
 const EventSchema = z.object({
   $collectionId: z.string(),
@@ -8,12 +8,14 @@ const EventSchema = z.object({
   $permissions: z.array(z.string()),
   $updatedAt: z.string().datetime(),
   description: z.string(),
+  retrieval_location: z.string().optional(),
+  address: z.string().optional(),
   id_device: z.string(),
   is_alert_on: z.boolean(),
   last_location: z.tuple([z.number(), z.number()]),
   time_event: z.string().datetime(),
   type: z.string(),
   id_district: z.string().optional(),
-});
+})
 
-export default EventSchema;
+export default EventSchema
