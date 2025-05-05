66bf8102f317b0b7fd0b03fbe689a2a0e9d7c94e
KelvynLenis <kmartinslenis30@hotmail.com>
2025-05-05 15:09:49 -0300

    fix: remove console.log

--- components/OccurrenceDetails.tsx
+++ components/OccurrenceDetails.tsx
@@ -41,7 +41,7 @@ export function OccurrenceDetails({ occurrence }: RecoverDeviceFormProps) {
 
   async function fetchEvents() {
     try {
-      const events = await listAllEvents(occurrence?.device.$id!)(events.length)
+      const events = await listAllEvents(occurrence?.device.$id!)
 
       setEvents(events)
     } catch (error) {
