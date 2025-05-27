dce73fc70fccb63bc7996e87a14cfb2f36712e07
KelvynLenis <kmartinslenis30@hotmail.com>
2025-05-27 15:21:04 -0300

    build: slug for mobile build

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -4,7 +4,7 @@ import 'dotenv/config';
 export default {
   expo: {
     name: "procuraai",
-    slug: "procura-ai",
+    slug: "mobile",
     version: "1.0.0",
     orientation: "portrait",
     icon: "./assets/images/logo.png",
