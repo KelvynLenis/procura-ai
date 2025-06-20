4b6c62ed0c3e39de7670b38024761aef64b060e8
KelvynLenis <kmartinslenis30@hotmail.com>
2025-06-20 15:27:25 -0300

    feat: increase district counter on submit

---
+++ mobile/functions/districts/get-neighborhood-id.ts
@@ -0,0 +1,37 @@
+export async function getNeighborhoodId(codNeighborhood: number): Promise<string> {
+  const params = new URLSearchParams({
+    'queries[0]': JSON.stringify({
+      method: 'equal',
+      attribute: 'cod_neighborhood',
+      values: [codNeighborhood],
+    }),
+  })
+
+  try {
+    const response = await fetch(
+      `${process.env.EXPO_PUBLIC_API_URL}/databases/${process.env.EXPO_PUBLIC_DATABASE_ID}/collections/${process.env.EXPO_PUBLIC_COLLECTION_DISTRICT}/documents?${params.toString()}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Appwrite-Project': `${process.env.EXPO_PUBLIC_APP_WRITE_PROJECT_ID}`,
+        },
+        cache: 'no-store',
+      }
+    )
+
+    if (!response.ok) {
+      throw new Error('Erro ao buscar bairro')
+    }
+
+    const result = await response.json()
+    if (!result.documents || result.documents.length === 0) {
+      throw new Error('Bairro não encontrado')
+    }
+
+    return result.documents[0].$id
+  } catch (error) {
+    console.error('Erro ao buscar bairro:', error)
+    throw error
+  }
+} 
