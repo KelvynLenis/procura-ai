17412e531d23726e4910049aca1e280b11cb545f
KelvynLenis <kmartinslenis30@hotmail.com>
2025-06-25 15:45:28 -0300

    feat: new header

--- mobile/app/(tabs)/profile.tsx
+++ mobile/app/(tabs)/profile.tsx
@@ -3,20 +3,23 @@ import React from 'react'
 import { Stack } from 'expo-router'
 import Header from '@/components/Header'
 import ProfileForm from '@/components/Forms/ProfileForm'
+import ProtectedRoute from '@/components/ProtectedRoute'
 
 export default function Profile() {
   return (
     <>
-      <Stack.Screen
-        options={{
-          header: () => (
-            <Header title="Editar Perfil" />
-          ),
-        }}
-      />
-      <View className='bg-[#F2F8FD] w-screen h-screen p-2'>
-        <ProfileForm />
-      </View>
+      <ProtectedRoute>
+        <Stack.Screen
+          options={{
+            header: () => (
+              <Header title="Editar Perfil" />
+            ),
+          }}
+        />
+        <View className='bg-[#F2F8FD] w-screen h-screen p-2'>
+          <ProfileForm />
+        </View>
+      </ProtectedRoute>
     </>
   )
 }
