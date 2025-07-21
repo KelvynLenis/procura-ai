e958ed1a0e27ac7ed95efe0ed5bef907593e9729
KelvynLenis <kmartinslenis30@hotmail.com>
2025-07-21 16:39:52 -0300

    feat: add geocoding control to map

---
+++ web/app/api/autocomplete/route.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+
+export async function GET(req: NextRequest) {
+  const input = req.nextUrl.searchParams.get('input');
+
+  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_API_KEY!
+
+  const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${input}&key=${apiKey}&components=country:br`;
+
+  const res = await fetch(url);
+  const data = await res.json();
+
+  return NextResponse.json(data);
+}
