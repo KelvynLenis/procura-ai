6e2a356f9595ac8e434cb466b2e10de2d57aa706
KelvynLenis <kmartinslenis30@hotmail.com>
2025-07-29 16:21:32 -0300

    fix: footer

--- web/components/Footer.tsx
+++ web/components/Footer.tsx
@@ -16,7 +16,7 @@ export function Footer({ light }: { light?: boolean }) {
   return (
     pathname !== 'map/ocorrencias' &&
     (
-      <footer className={cn("w-full relative bottom-0 md:mt-0 flex items-center justify-start z-[20] py-4", light ? 'bg-white' : 'bg-primary')}>
+      <footer className={cn("w-full relative bottom-0 md:mt-0 flex items-center justify-start z-[20]", light ? 'bg-white' : 'bg-primary py-4')}>
         <div className={cn('w-full h-20 flex items-center justify-start pl-9', light ? ' overflow-hidden': 'justify-center hidden lg:flex md:justify-start py-0 px-8 h-14 md:h-fit')}>
           {
             light ? (
