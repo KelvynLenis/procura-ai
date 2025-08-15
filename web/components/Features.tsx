fc84b051e0ca8f6068b2da7300f5e91680978218
KelvynLenis <kmartinslenis30@hotmail.com>
2025-08-15 10:13:31 -0300

    fix: button being overlaped

--- web/components/Features.tsx
+++ web/components/Features.tsx
@@ -120,7 +120,7 @@ export function Features() {
             Cadastre-se
           </Button>
         </Link>
-        <span className="text-lg md:text-2xl font-bold flex flex-col items-center text-procura-ai-black 3xl:mb-20">
+        <span className="text-lg md:text-2xl font-bold flex flex-col items-center text-procura-ai-black lg:mb-10 3xl:mb-20">
           Disponível em breve nas principais lojas de aplicativos
         <Link href={process.env.NEXT_PUBLIC_APK_DOWNLOAD_LINK!}>
           <button type='button' className='rounded-lg px-4 pt-2 pb-1.5 text-white bg-none'>
