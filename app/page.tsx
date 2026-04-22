a3ae460415622dbcd7efa601c785f38becd8ba15
KelvynLenis <kmartinslenis30@hotmail.com>
2026-04-22 08:35:13 -0300

    fix: img not defined

--- app/page.tsx
+++ app/page.tsx
@@ -33,6 +33,7 @@ import { FaFacebookF } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaYoutube } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa6";
+import sicLogo from "../../assets/icons/sic_logo.png";
 
 export default function Landing() {
   const renderStepContent = (step: QuestionStep) => (
